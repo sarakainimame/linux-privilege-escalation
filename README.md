cd232878aead7756c18a6dbb38bfa9ac1e797f31
sarakainimame <sarakainimame@gmail.com>
2021-04-19 08:17:16 +0200

    adding first lines

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # linux-privilege-escalation
 a tool designed for privilege escalation in Linux
+
+# this tool is based on the Daniel Lowrie Youtube course: https://www.youtube.com/watch?v=gc2cFwT_Fd0&t=1081s
---
+++ priv-esc-linux.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env/ python3
+
+
+### This is an automation tool for privilege escalation in Linux
+
+# It does:
+#	+ OS, Release, and Kernel information gathering
+#	+ Root service check
+#	+ SUID-GUID check ('can this user sudo anything?')
